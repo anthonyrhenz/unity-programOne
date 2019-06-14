f2f39ae4030a21f267184e84ba55613aec6b0543
Anthony Bennett <anton8000@live.com>
2019-06-14 05:15:32 +0100

    Committing existing files
    Adding all the files that I have been working on already locally

---
+++ Assets/Scripts/CamControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour { 
+
+    public float NaturalLag = 0f;
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position; 
+    }
+
+    //Late Update is called at the end
+    void LateUpdate()
+    {
+        Vector3 desiredPos = player.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPos, Time.deltaTime * NaturalLag);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    //definitions 
+    public Rigidbody rb;
+
+    public Text countText;
+    public Text winText;
+
+    public float speed = 1f;
+
+    private int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        setCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate() //called before physics calcs
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            setCountText();
+        }
+    }
+
+    void setCountText ()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count == 4)
+        {
+            winText.text = "You win?";
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
