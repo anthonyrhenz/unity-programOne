c26e0b8338e24c12d9a59b58104a341dcf31d5b9
Anthony Bennett <anton8000@live.com>
2019-06-14 11:10:41 +0100

    Switched Controller to PlayerController

--- Assets/Scripts/ThirdPersonMover.cs
+++ Assets/Scripts/ThirdPersonMover.cs
@@ -6,7 +6,10 @@ using System;
 
 public class ThirdPersonMover : MonoBehaviour
 {
+    public CharacterController controller;
     public float speed = 1f;
+    public float jump = 1f;
+    private Vector3 moveDirection = Vector3.zero;
     public Rigidbody rb;
     Animator animator;
     public Text dbgtxt;
@@ -23,26 +26,38 @@ public class ThirdPersonMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
-        jmp = Input.GetKey(KeyCode.Space);
+        
 
-        //jump
-        if (jmp) { animator.SetBool("jump", true); }
-        else { animator.SetBool("jump", false); }
+        //general input movement
+        if (controller.isGrounded)
+        {
+            //get inputs
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+            jmp = Input.GetKey(KeyCode.Space);
+
+            //jump
+            if (jmp) { animator.SetBool("jump", true); }
+            else { animator.SetBool("jump", false); }
+
+        }
+        //apply move
+        controller.Move(new Vector3(h * Time.deltaTime * speed, 0, v * Time.deltaTime * speed));
+
         //sprint
         if (Input.GetKey(KeyCode.LeftShift)) { if (speed < 10) { speed += 0.02f; } }
         else { if (speed>3) { speed -= 0.05f; } }
 
+        //animate
         animator.SetFloat("speed", (Math.Abs(h) + Math.Abs(v))*speed);
-        
 
         //rb.AddForce(new Vector3(h, 0.0f, v) * speed);
-        transform.Translate(h*Time.deltaTime*speed, 0f, v*Time.deltaTime*speed );
+        // transform.Translate(h*Time.deltaTime*speed, 0f, v*Time.deltaTime*speed );
     }
 
     void LateUpdate()
     {
+        //debugging
         var debugstring = ((h + v)*speed).ToString() + "\n" + "dTime:" + Time.deltaTime.ToString() ;
         dbgtxt.text = debugstring;
     }
