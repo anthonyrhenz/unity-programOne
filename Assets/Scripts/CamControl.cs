956fbb8a23a9e37878979ce4b713ea954b4a46ad
Anthony Bennett <anton8000@live.com>
2019-06-15 17:28:04 +0100

    Orbital Camera
    Adding mouse control with toggles

--- Assets/Scripts/CamControl.cs
+++ Assets/Scripts/CamControl.cs
@@ -8,6 +8,19 @@ public class CamControl : MonoBehaviour {
     public GameObject player;
     private Vector3 offset;
 
+    public bool MouseControl = false;
+    public bool MouseMove = false;
+    public bool MouseRotate = true;
+    public bool Orbital = false;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    public float HorizontalSense = 1.0f;
+    public float VerticalSense = 1.0f;
+
+    public float Sensitivity = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +30,33 @@ public class CamControl : MonoBehaviour {
     //Late Update is called at the end
     void LateUpdate()
     {
+
+        if (MouseControl)
+        {
+            if (MouseMove)
+            {
+                transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * Sensitivity, 0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * Sensitivity);
+            }
+            if (MouseRotate & !Orbital)
+            {
+                yaw += HorizontalSense * Input.GetAxis("Mouse X");
+                pitch -= VerticalSense * Input.GetAxis("Mouse Y");
+
+                transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+            }
+            if (MouseRotate & Orbital)
+            {
+                Quaternion yaw = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * HorizontalSense, Vector3.up);
+                Quaternion pitch = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * VerticalSense, Vector3.left);
+                transform.LookAt(player.transform);
+                offset = yaw * offset;
+                offset = pitch * offset;
+                
+            }
+        }
+
+        //apply all transforms with a lerp
         Vector3 desiredPos = player.transform.position + offset;
-        transform.position = Vector3.Lerp(transform.position, desiredPos, Time.deltaTime * NaturalLag);
+        transform.position = Vector3.Slerp(transform.position, desiredPos, Time.deltaTime * NaturalLag);
     }
 }
